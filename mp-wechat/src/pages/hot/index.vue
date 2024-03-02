2a641e8595d15954ed3d5cf73b07a974b8a8bcb0
星星魔法术 <xingxingmofashu@outlook.com>
2024-03-02 11:15:08 +0800

    feat: hot latest random pages

--- mp-wechat/src/pages/hot/index.vue
+++ mp-wechat/src/pages/hot/index.vue
@@ -1,7 +1,59 @@
 <script setup lang="ts">
 import { useTabBar } from "@/composables/useTabBar";
+import { useHot } from "@/composables/wallhaven";
+import { ref } from "vue";
+import Thumbs from "@/components/thumbs/index.vue";
+import type { WallHavenThumb } from "~/types";
+import { watch } from "vue";
+import Header from "@/components/header/index.vue";
+
+const topList = ref<Array<WallHavenThumb>>([]);
+const page = ref(1);
+
 useTabBar(3);
+
+watch(page, async () => {
+    const data = await useHot(page.value)
+    topList.value.push(...data)
+}, {
+    immediate: true
+})
+
 </script>
 <template>
-    <div>Hot</div>
+    <div class="container">
+        <Header>
+            <template>
+                <div class="listing-header">{{ 'Latest' }}</div>
+            </template>
+        </Header>
+        <scroll-view scroll-y class="content" @scrolltolower="page++;">
+            <Thumbs :data="topList" />
+        </scroll-view>
+    </div>
 </template>
+<style scoped lang="scss">
+.container {
+    height: 100vh;
+    color: white;
+    font-family: 'Courier New', Courier, monospace;
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        overflow: auto;
+        max-height: 76vh;
+        position: relative;
+
+    }
+}
+
+.listing-header {
+    height: 2.5rem;
+    line-height: 2.5rem;
+    padding-left: 2rem;
+    font-size: 1.5rem;
+    font-weight: 600;
+    background-image: radial-gradient(400px 80px at 0px top, rgba(183, 96, 240, .3), transparent);
+}
+</style>
--- mp-wechat/src/pages/latest/index.vue
+++ mp-wechat/src/pages/latest/index.vue
@@ -1,7 +1,59 @@
 <script setup lang="ts">
 import { useTabBar } from "@/composables/useTabBar";
+import { useLatest } from "@/composables/wallhaven";
+import { ref } from "vue";
+import Thumbs from "@/components/thumbs/index.vue";
+import type { WallHavenThumb } from "~/types";
+import { watch } from "vue";
+import Header from "@/components/header/index.vue";
+
+const topList = ref<Array<WallHavenThumb>>([]);
+const page = ref(1);
+
 useTabBar(2);
+
+watch(page, async () => {
+    const data = await useLatest(page.value)
+    topList.value.push(...data)
+}, {
+    immediate: true
+})
+
 </script>
 <template>
-    <div>Latest</div>
+    <div class="container">
+        <Header>
+            <template>
+                <div class="listing-header">{{ 'Latest' }}</div>
+            </template>
+        </Header>
+        <scroll-view scroll-y class="content" @scrolltolower="page++;">
+            <Thumbs :data="topList" />
+        </scroll-view>
+    </div>
 </template>
+<style scoped lang="scss">
+.container {
+    height: 100vh;
+    color: white;
+    font-family: 'Courier New', Courier, monospace;
+
+    .content {
+        display: flex;
+        flex-direction: column;
+        overflow: auto;
+        max-height: 76vh;
+        position: relative;
+
+    }
+}
+
+.listing-header {
+    height: 2.5rem;
+    line-height: 2.5rem;
+    padding-left: 2rem;
+    font-size: 1.5rem;
+    font-weight: 600;
+    background-image: radial-gradient(400px 80px at 0px top, rgba(183, 96, 240, .3), transparent);
+}
+</style>
