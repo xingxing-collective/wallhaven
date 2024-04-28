1d1109661c1b2f99cf7d610f2bf0729ac7be3699
星星魔法术 <xingxingmofashu@outlook.com>
2024-04-28 16:31:40 +0800

    fix: page disable scroll

--- mp-wechat/src/App.vue
+++ mp-wechat/src/App.vue
@@ -3,6 +3,7 @@ import 'virtual:uno.css'
 </script>
 <style>
 page {
+  overflow: hidden;
   object-fit: fill;
   background-size: cover;
   background-image: url(https://7072-prod-xingxing-music-7cdbce8de59b-1318958897.tcb.qcloud.la/wallhaven.jpg?sign=ef0cdd6f98231af3267ac11697b28f95&t=1708936774);
