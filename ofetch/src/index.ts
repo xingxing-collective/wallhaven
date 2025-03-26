dbb254ddc39b7c841d2dd2e7788adaa4e931e358
星星魔法术 <xingxingmofashu@outlook.com>
2025-03-26 18:00:13 +0800

    refactor: 重构项目 (#7)

--- ofetch/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { createFetch } from './fetch';
-export type * from './types'
-
-const fetch = uni.request;
-
-export const ofetch = createFetch({ fetch });
-
-export const $fetch = ofetch;
--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,11 @@ import Unocss from "unocss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    fs: {
-      allow: [".."]
-    }
-  },
   plugins: [uni(), Unocss()],
   resolve: {
     alias: {
       "@": "./src",
       "~": "./"
     },
-  }
+  },
 });
